9c89b64dd61af49add18cba7f20fbcc1629a1194
Alexey Yakovenko <mr.mnogo@gmail.com>
2025-02-10 09:23:20 +0300

    add dinamic kb

--- bot/booking/add_booking.py
+++ bot/booking/add_booking.py
@@ -3,36 +3,61 @@ from aiogram.types import CallbackQuery
 from aiogram_dialog import DialogManager, Dialog, Window
 from aiogram_dialog.widgets.common import ManagedScroll, Scroll
 from aiogram_dialog.widgets.kbd import Button, Group, PrevPage, NextPage, Cancel, ListGroup, SwitchInlineQuery, \
-    ScrollingGroup
+    ScrollingGroup, Select
 from aiogram_dialog.widgets.text import Const, Format
 from bot.booking.state import BookingState
 from bot.dao.dao import TableDAO
 from bot.dao.database import async_session_maker
 
 
-async def get_all_tables():
+async def get_all_tables(**kwargs):
     async with async_session_maker() as session:
         tables = await TableDAO(session).find_all()
     return {"tables": [i.to_dict() for i in tables]}
 
 
-def create_button(**kwargs):
-    return Button(
-        Const("Go"),
-        id="go",
-    )
+async def on_table_selected(
+        callback: CallbackQuery,
+        select: Select,
+        manager: DialogManager,
+        item_id: str
+):
+    # Обработка выбора стола
+    await callback.answer(f"Вы выбрали стол с ID: {item_id}")
 
 
-def create_count_table():
-    list_group = [Button(Const(str(i)), id=str(i)) for i in range(1, 7)]
-    list_group.append(Button(Const("Отмена"), id="cancel"))
-    return list_group
-
+# get_count = Window(
+#     Const("Выберите стол:"),
+#     ScrollingGroup(
+#         Select(
+#             Format("Стол {item[id]} ({item[capacity]} мест)"),
+#             id="table_select",
+#             item_id_getter=lambda item: str(item["id"]),
+#             items="tables",
+#             on_click=on_table_selected,
+#         ),
+#         id="tables_scrolling",
+#         width=2,
+#         height=5,
+#     ),
+#     getter=get_all_tables,
+#     state=BookingState.count,
+# )
 
 get_count = Window(
-    Const("Выберите количество гостей: "),
-    Group(*create_count_table(), width=2),
-    state=BookingState.count
+    Const("Выберите стол:"),
+    Group(
+        Select(
+            Format("Стол {item[id]} ({item[capacity]} мест)"),
+            id="table_select",
+            item_id_getter=lambda item: str(item["id"]),
+            items="tables",
+            on_click=on_table_selected,
+        ),
+        width=2,
+    ),
+    getter=get_all_tables,
+    state=BookingState.count,
 )
 
 dialog = Dialog(get_count)
