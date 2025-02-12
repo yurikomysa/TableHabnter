e600b024e5d3b42b04ba8dd9d6c377aee7d3dd31
Alexey Yakovenko <mr.mnogo@gmail.com>
2025-02-13 00:00:28 +0300

    clear code

--- app/scheduler/tasks.py
+++
@@ -1,8 +0,0 @@
-from app.bot.create_bot import bot
-from app.config import broker, settings
-
-
-@broker.subscriber("new_booking")
-async def send_booking_msg(msg: str):
-    for admin in settings.ADMIN_IDS:
-        await bot.send_message(admin, text=msg)
